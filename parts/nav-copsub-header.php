8d5724770da47857ff2e95e706967ce758b296b2
post <post@rasmusa.dk>
2016-07-31 06:25:59 -0400

    modified logo header

--- parts/nav-copsub-header.php
+++ parts/nav-copsub-header.php
@@ -1,10 +1,10 @@
 <!-- Copsub header -->
 
-<div class="row expanded">
+<div class="row expanded cs-grey-bg hide-for-small-only" id="logo-header">
 	<div class="small-4  columns float-left">
 		<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/cslogo.png"></a>
 	</div>
 	<div class="small-8  columns">
-		jjj
+		<!-- Social media links -->
 	</div>
 </div>
