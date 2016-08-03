b8f424fbaa732eb8129c882b7f4ea7b3d5f41924
post <post@rasmusa.dk>
2016-08-03 14:41:44 -0400

    header update

--- parts/nav-copsub-header.php
+++ parts/nav-copsub-header.php
@@ -1,10 +1,15 @@
 <!-- Copsub header -->
 
-<div class="row expanded cs-grey-bg hide-for-small-only" id="logo-header">
-	<div class="small-4  columns float-left">
-		<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/cslogo.png"></a>
-	</div>
-	<div class="small-8  columns">
-		<!-- Social media links -->
+
+<div id="logo-header" class="cs-grey-bg">
+
+
+	<div class="row column  hide-for-small-only">
+		<div class="small-4  columns float-left">
+			<a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/cslogo.png"></a>
+		</div>
+		<div class="small-8  columns">
+			<!-- Social media links -->
+		</div>
 	</div>
 </div>
--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -1,16 +1,21 @@
 <!-- By default, this menu will use off-canvas for small
 	 and a topbar for medium-up -->
 
-<div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
-		 <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
-		 <li><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/cslogo.png"></a></li>
-			<!-- <li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li> -->
-		</ul>
-	</div>
-</div>
 
+
+<div class="top-bar" id="top-bar-menu">
+	<div class="coulmn row">
+
+		<div class="top-bar-left show-for-medium" style="text-align:center;display: inline-block;">
+			<?php joints_top_nav(); ?>
+		</div>
+		<div class="top-bar-right float-right show-for-small-only">
+			<ul class="menu">
+				<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+				<li><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/cslogo.png"></a></li>
+				<!-- <li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li> -->
+			</ul>
+		</div>
+	</div>
+
+</div>
