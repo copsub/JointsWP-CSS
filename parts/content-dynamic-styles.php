fe7199fd409a14809ebf26f2427ac0cc45545ef3
post <post@rasmusa.dk>
2016-08-24 17:13:33 -0400

    dynamic styles, acf and template

---
+++ parts/content-dynamic-styles.php
@@ -0,0 +1,158 @@
+<style>
+	
+<?php 
+	$mainsection_index = 0;
+// check for rows (parent repeater)
+if( have_rows('mainsection', $post->ID ) ):
+	// loop through rows (parent repeater)
+	while( have_rows('mainsection', $post->ID ) ): the_row(); 
+		$background_image = get_sub_field('background_image');
+		$background_image_ratio = ($background_image[height]/$background_image[width])*100;
+		$background_color = get_sub_field('background_color');
+		$margin_top = get_sub_field('margin_top');
+		$margin_bottom = get_sub_field('margin_bottom');
+		?>
+			.wrapper_main_section_<?php echo $mainsection_index ?> {
+				background-image: url('<?php echo $background_image[url] ?>');
+			}	
+			.wrapper_main_section_<?php echo $mainsection_index ?>:after {
+			  padding-top: <?php echo $background_image_ratio; ?>%;
+				display: block;
+  			content: '';
+			}	
+			.margin_top_main_section_<?php echo $mainsection_index ?> {
+				background-color: <?php echo $background_color; ?>;
+				height: <?php echo $margin_top; ?>vw;
+			}
+			.margin_bottom_main_section_<?php echo $mainsection_index ?> {
+				background-color: <?php echo $background_color; ?>;
+				height: <?php echo $margin_bottom; ?>vw;
+			}
+		<?php	
+		// check for rows (sub repeater)
+	  $overlaysection_index = 0;
+		if( have_rows('overlay') ): 
+			// loop through rows (sub repeater)
+			while( have_rows('overlay') ): the_row();
+				$sub_section_active = get_sub_field('sub_section_active');
+				$background_image = get_sub_field('background_image');
+				$background_image_hover = get_sub_field('background_image_hover');
+				$background_position = get_sub_field('background_position');
+				$background_color = get_sub_field('background_color');
+				$background_size = get_sub_field('background_image_size');
+				$left_position = get_sub_field('left_position');
+				$overlay_width = get_sub_field('overlay_width');
+				$top_position = get_sub_field('top_position');
+				$overlay_height = get_sub_field('overlay_height');
+				$overlay_padding = get_sub_field('overlay_padding');
+				$text_color = get_sub_field('text_color');
+				$font_size = get_sub_field('font_size');
+				$font_weight = get_sub_field('font_weight');
+				$line_height = get_sub_field('line_height');
+				$text_align = get_sub_field('text_align');
+
+				switch ($text_align) {
+    			case 'lefttop':
+						$htext_align = 'left';
+						$vertical_align = 'top';
+					break;
+    			case 'leftmiddle':
+						$htext_align = 'left';
+						$vertical_align = 'middle';
+					break;
+    			case 'leftbottom':
+						$htext_align = 'left';
+						$vertical_align = 'bottom';
+					break;
+    			case 'centertop':
+						$htext_align = 'center';
+						$vertical_align = 'top';
+					break;
+    			case 'centermiddle':
+						$htext_align = 'center';
+						$vertical_align = 'middle';
+					break;
+    			case 'centerbottom':
+						$htext_align = 'center';
+						$vertical_align = 'bottom';
+					break;
+					case 'righttop':
+						$htext_align = 'right';
+						$vertical_align = 'top';
+					break;
+    			case 'rightmiddle':
+						$htext_align = 'right';
+						$vertical_align = 'middle';
+					break;
+    			case 'rightbottom':
+						$htext_align = 'right';
+						$vertical_align = 'bottom';
+					break;
+					default:
+				}	
+	
+	
+				$hover_layout = get_sub_field('hover_layout');
+
+
+			// display each item as a list - with a class of completed ( if completed )?>
+				.main_front_section_<?php echo $mainsection_index ?>_overlay_<?php echo $overlaysection_index ?> {
+  				position: absolute;
+  				top: <?php echo $top_position ?>%;
+  				bottom: <?php echo (100-$overlay_height-$top_position) ?>%;
+  				right: <?php echo (100-$overlay_width-$left_position) ?>%;
+  				left: <?php echo $left_position ?>%;
+					color: <?php echo $text_color ?>;
+					background-color: <?php echo $background_color ?>;
+					background-image: url('<?php echo $background_image['url'] ?>');
+					background-repeat: no-repeat;
+					background-position: <?php echo $background_position ?>;
+					background-size: <?php echo $background_size ?>%;
+					font-size: <?php echo $font_size ?>vw;
+					font-weight: <?php echo $font_weight ?>;
+					line-height: <?php echo $line_height ?>;
+					text-align: <?php echo $htext_align ?>;
+					vertical-align: <?php echo $vertical_align ?>;
+					height: <?php echo $overlay_height ?>%;
+					padding: <?php echo $overlay_padding ?>;					
+				}
+				.main_front_section_<?php echo $mainsection_index ?>_overlay_<?php echo $overlaysection_index ?>:hover {
+				<?php	
+				switch ($hover_layout) {
+    			case 'none':
+					?>
+        	<?php 
+					break;
+    			case 'csgrey-white':
+					?>
+						background-color: #e7e7e7;
+						color: #FFFFFF;
+        	<?php         		
+        	break;    		
+					case 'csgrey-black':
+					?>
+						background-color: #e7e7e7;
+						color: #000000;
+        	<?php         		
+        	break;
+    			case 'image': 
+					?>
+						background-image: url('<?php echo $background_image_hover['url'] ?>');
+        	<?php         		
+        	break;
+					default:
+				}	
+				?>	
+					
+					
+
+				}
+			<?php 
+	    $overlaysection_index++;
+	    endwhile; 
+		endif; 	
+		$mainsection_index++;
+	endwhile;  
+endif;  ?>
+
+</style>
--- template-full-width-sections.php
+++ template-full-width-sections.php
@@ -25,7 +25,10 @@ global $post;
 					$section_active = get_sub_field('section_active');
 					if ($section_active):
 					$background_image = get_sub_field('background_image');
+					$margin_top = get_sub_field('margin_top');
+					$margin_bottom = get_sub_field('margin_bottom');
 					?>
+					<?php if ($margin_top > 0):?><div class="margin_top_main_section_<?php echo $mainsection_index ?>"></div><?php endif; ?>
 					<div class=" full-width-background wrapper_main_section_<?php echo $mainsection_index ?>">
 					<?php 
 					// check for rows (sub repeater)
@@ -35,18 +38,29 @@ global $post;
 						while( have_rows('overlay') ): the_row();
 							$sub_section_active = get_sub_field('sub_section_active');
 							$text = get_sub_field('text');
+							$link_type = get_sub_field('link_type');
+							$link_on_site = get_sub_field('link_on_site');
+							$link_off_site = get_sub_field('link_off_site');
+							if ($link_type == 'onsite'):
+							 $link = $link_on_site;
+							else:
+							 $link = $link_off_site;
+							endif;
 							if ($sub_section_active): 
 							// display each item as a list - with a class of completed ( if completed )
 							?>
-							<div class="main_front_section_<?php echo $mainsection_index ?>_overlay_<?php echo $overlaysection_index ?>">
+							<?php if ($link_type <> 'none'): ?> <a href="<?php echo $link ?>"> <?php endif; //Loverlay Link?> 
+								<div class="main_front_section_<?php echo $mainsection_index ?>_overlay_<?php echo $overlaysection_index ?>">
 								<?php echo $text; ?>
 							</div>
+							<?php if ($link_type <> 'none'): ?>	</a> <?php endif; //Overlay link?>
 						<?php
 							endif;
 	          $overlaysection_index++;
 	          endwhile; ?>
 					<?php endif; //if( get_sub_field('items') ): ?>
 					</div>
+					<?php if ($margin_bottom > 0):?><div class="margin_bottom_main_section_<?php echo $mainsection_index ?>"></div><?php endif; ?>
 					<?php endif; ?>
 				
 					<?php $mainsection_index++ ?>
